2f96b1f46092e291244086ff0341657d20ea2c67
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-04-19 16:35:39 +0300

    1

--- hw3/1.py
+++ hw3/1.py
@@ -42,28 +42,40 @@ def main():
         {"name": "Marry", "age": 33, "number": 3},
         {"name": "Cavin", "age": 33, "number": 12},
     ]
+    
 
-    players_33: list[dict] = players_find(players=team, field="age", value=33)
-    print(players_33)
-    print()
     options = ["repr", "add", "del", "find", "get", "exit"]
 
     while True:
-        if not (user_input := input(f"Enter your choice {options}:")):
+        if not (user_input := input(f"Enter your choice {options}: \n")):
             break
 
         if user_input == "add":
             name = input('Введите имя игрока: ')
-            age = input('Введите возраст игрока: ')
+            age = int(input('Введите возраст игрока: '))
             number = int(input('Введите номер игрока: '))
             player = {'name': name, 'age': age, 'number': number}
 
             players_add(team, player)
+            print('Игрок успешно добавлен!')
+
         elif user_input == 'find':
             field = input('Введите переменную котороя может присутствовать в списке: ')
             value = int(input('Введите значение этой переменной: '))
+            print(players_find(team, field, value))
+        
+        elif user_input == 'get':
+            pass
 
-            players_find(team, field, value)
+        elif user_input == 'del':
+            name = input('Введите имя игрока: ')
+            print(players_del(team, name))
+
+        elif user_input == 'repr':
+            players_repr(team)
+        
+        elif user_input == 'exit':
+            break
             
 
 
