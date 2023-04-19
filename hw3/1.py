2e8be55f25a9646033290277b75bee25aedd8717
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-04-19 16:16:36 +0300

    1

--- hw3/1.py
+++ hw3/1.py
@@ -1,28 +1,35 @@
 from typing import Any
 
 
-def players_repr(players: list[dict], verbose: bool) -> None:
-    if verbose:
-        print(">>>> TEAM:")
+def players_repr(players: list[dict]) -> None:
+    print(">>>> TEAM:")
 
     for player in players:
         print(f"{player['name']=}, {player['age']=}")
 
 
 def players_add(players: list[dict], player: dict) -> list[dict]:
-    name = input('Введите имя игрока: ')
-    age = input('Введите возраст игрока: ')
-    number = int(input('Введите номер игрока: '))
+    players.append(player)
 
-    pass
-
+    return players
+    
 
 def players_del(players: list[dict], name: str) -> list[dict]:
-    pass
+    for player in players:
+        if player['name'] == name:
+            players.remove(player)
+    
+    return players
 
 
 def players_find(players: list[dict], field: str, value: Any) -> list[dict]:
-    pass
+    playing_people_with_this_attribute = []
+
+    for player in players:
+        if player[field] == value:
+            playing_people_with_this_attribute.append(player)
+
+    return playing_people_with_this_attribute
 
 
 def players_get_by_name(players: list[dict], name: str) -> dict | None:
@@ -37,7 +44,8 @@ def main():
     ]
 
     players_33: list[dict] = players_find(players=team, field="age", value=33)
-
+    print(players_33)
+    print()
     options = ["repr", "add", "del", "find", "get", "exit"]
 
     while True:
@@ -45,8 +53,18 @@ def main():
             break
 
         if user_input == "add":
-            # players_add(...)
-            pass
+            name = input('Введите имя игрока: ')
+            age = input('Введите возраст игрока: ')
+            number = int(input('Введите номер игрока: '))
+            player = {'name': name, 'age': age, 'number': number}
+
+            players_add(team, player)
+        elif user_input == 'find':
+            field = input('Введите переменную котороя может присутствовать в списке: ')
+            value = int(input('Введите значение этой переменной: '))
+
+            players_find(team, field, value)
+            
 
 
 
