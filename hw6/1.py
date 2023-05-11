d6a21fc7cdab82ba1374c04b844a9995d3cbbdce
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-05-11 23:09:52 +0300

    1

--- hw6/1.py
+++ hw6/1.py
@@ -5,26 +5,23 @@ import requests
 
 # CPU-bound task (heavy computation)
 def encrypt_file(path: str):
-    start_time = time.perf_counter()
     print(f"Processing image from {image_url} in process {os.getpid()}")
     # Simulate heavy computation by sleeping for a while
     _ = [i for i in range(100_000_000)]
-    end_time = time.perf_counter()
-    print(f"{end_time - start_time:.4f} секунд")
 
 # I/O-bound task (downloading image from URL)
 def download_image(image_url):
-    start_time = time.perf_counter()
     print(f"Downloading image from {image_url} in thread {threading.current_thread().name}")
     response = requests.get(image_url)
     with open("image.jpg", "wb") as f:
         f.write(response.content)
-    end_time = time.perf_counter()
-    print(f"{end_time - start_time:.4f} секунд")
 
 try:
+    start_time = time.perf_counter()
     encrypt_file("rockyou.txt")
     download_image("https://picsum.photos/1000/1000")
+    end_time = time.perf_counter()
+    print(f"{end_time - start_time:.4f} секунд")
     print(f"Time taken for encryption task: {encryption_counter}, I/O-bound task: {download_counter}, Total: {total} seconds")
 except Exception as e:
     print(f"Error occurred: {e}")
