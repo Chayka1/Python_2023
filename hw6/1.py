c932e01512fe1143d5631d92aa396eccb00e9142
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-05-12 20:48:36 +0300

    1

--- hw6/1.py
+++ hw6/1.py
@@ -1,27 +1,40 @@
-import os
+import multiprocessing
 import threading
 import time
 import requests
 
-# CPU-bound task (heavy computation)
+
 def encrypt_file(path: str):
-    print(f"Processing image from {image_url} in process {os.getpid()}")
-    # Simulate heavy computation by sleeping for a while
     _ = [i for i in range(100_000_000)]
 
-# I/O-bound task (downloading image from URL)
+
 def download_image(image_url):
     print(f"Downloading image from {image_url} in thread {threading.current_thread().name}")
     response = requests.get(image_url)
     with open("image.jpg", "wb") as f:
         f.write(response.content)
 
-try:
-    start_time = time.perf_counter()
-    encrypt_file("rockyou.txt")
-    download_image("https://picsum.photos/1000/1000")
-    end_time = time.perf_counter()
-    print(f"{end_time - start_time:.4f} секунд")
-    print(f"Time taken for encryption task: {encryption_counter}, I/O-bound task: {download_counter}, Total: {total} seconds")
-except Exception as e:
-    print(f"Error occurred: {e}")
+
+if __name__ == "__main__":
+    try:
+        encrypt_file("rockyou.txt")
+        download_image("https://picsum.photos/1000/1000")
+
+        encryption_process = multiprocessing.Process(target=encrypt_file, args=("rockyou.txt",))
+        download_thread = threading.Thread(target=download_image, args=("https://picsum.photos/1000/1000",))
+
+        start_encrypt_file = time.perf_counter()
+        encryption_process.start()
+        encryption_process.join()
+        end_time_encrypt_file = time.perf_counter() - start_encrypt_file
+
+        start_download_image = time.perf_counter()
+        download_thread.start()
+        download_thread.join()
+        end_time_download_image = time.perf_counter() - start_download_image
+        
+        total = end_time_download_image + end_time_encrypt_file
+        
+        print(f"\nTime taken for encryption task: {end_time_encrypt_file:.4f}, I/O-bound task: {end_time_download_image:.4f}, Total: {total:.4f} seconds")
+    except Exception as e:
+        print(f"Error occurred: {e}")
